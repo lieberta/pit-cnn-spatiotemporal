16ab9e6677fc80e58d59be5a5e2a966a3430e605
lieberta <lieberta@hsu-hh.de>
2024-06-19 13:50:24 +0200

    first draft for dynamic pecnn

--- models.py
+++ models.py
@@ -41,6 +41,7 @@ class DecoderBlock_transposedcnn_cross(nn.Module):
         x = F.gelu(self.gn1(self.conv1(x)))
         x = F.gelu(self.gn2(self.conv2(x)))
         return x
+
 class PICNN_static(BaseModel):
     # this is a new version of CNN3D1D with cross connections, but only 2 blocks deep
     # maxpooling instead of step size and additional conv layers in each block
@@ -184,6 +185,130 @@ class PICNN_VAR(BaseModel):
         x[:, :, :, :, 0] = x_input[:, :, :, :, 0]
         x[:, :, :, :, -1] = x_input[:, :, :, :, -1]
         return x, mu, logvar
+
+class PICNN_dynamic(BaseModel):
+    # This is a Encoder-Decoder U-Net structured physics enhanced conv network with outputpadding for
+    # boundary conditions in the data and cross connections between encoder blocks and decoder blocks
+    # latent room in middle part enhanced with one layer of timestep
+
+    # 3 Encoder Blocks with: 3dconv / groupnormalization / 3dconv / groupnorm / avgpool
+    # Middle: 3dconv / groupnorm / 3dconv / groupnorm
+    # 3 Decoder Blocks with: tconv / groupnorm / tconv / groupnorm
+    # groupnumber in group normalization increases with number of outputchannels
+    def __init__(self,c = 8, loss_fn):
+        super(PICNN_dynamic, self).__init__(loss_fn=loss_fn)
+
+        # conv<i><j> with i represents blocknumber and j represents the index inside of the block
+        # one avgpool initilization for all blocks (no trainable parameter)
+
+        # Encoderblock 1
+        self.conv11 = nn.Conv3d(1, c, kernel_size=3, padding=1, padding_mode='reflect')
+        self.gn11 = nn.GroupNorm(num_groups=int(c / 8), num_channels=c)
+        self.conv12 = nn.Conv3d(out_c, out_c, kernel_size=3, padding=1, padding_mode='reflect')
+        self.gn12 = nn.GroupNorm(num_groups=int(c / 8), num_channels=c)
+        self.avgpool = nn.AvgPool3d(kernel_size=2, stride=2)
+
+        # Encoderblock 2
+        self.conv21 = nn.Conv3d(c, c*2, kernel_size=3, padding=1, padding_mode='reflect')
+        self.gn21 = nn.GroupNorm(num_groups=int(c*2 / 8), num_channels=c*2)  # nn.BatchNorm3d(out_c)
+        self.conv22 = nn.Conv3d(c*2, c*2, kernel_size=3, padding=1, padding_mode='reflect')
+        self.gn22 = nn.GroupNorm(num_groups=int(c*2 / 8), num_channels=c*2)
+        # avgpool in forward
+
+        # Encoderblock 3
+        self.conv31 = nn.Conv3d(c*2, c*4, kernel_size=3, padding=1, padding_mode='reflect')
+        self.gn31 = nn.GroupNorm(num_groups=int(c*4 / 8), num_channels=c*4)
+        self.conv32 = nn.Conv3d(c*4, c*4, kernel_size=3, padding=1, padding_mode='reflect')
+        self.gn32 = nn.GroupNorm(num_groups=int(c*4 / 8), num_channels=c*4)
+        # avgpool in forward
+
+        # Middle Block
+        self.conv1 = nn.Conv3d(c*4 + 1, c*8, kernel_size=3, padding=1, padding_mode='reflect')  # here +1 inputlayer filled with the current timestep
+        self.gn1 = nn.GroupNorm(num_groups=int(c), num_channels= c*8)                           # groups = int(c) because it would be c*8/8
+        self.conv2 = nn.Conv3d(c*8, c*8, kernel_size=3, padding=1, padding_mode='reflect')
+        self.gn2 = nn.GroupNorm(num_groups=int(c), num_channels=c*8)
+
+        # Decoderblock 1
+        self.up1 = nn.ConvTranspose3d(c*8, c*8, kernel_size= 2,stride=2, output_padding = 0)
+        self.conv41 = nn.Conv3d(c*8 + c*4, c*4, kernel_size=3, padding=1)                       # here additional input_channels for cross connections can be added
+        self.gn41 = nn.GroupNorm(num_groups=int(c*4/8), num_channels= c*4)
+        self.conv42 = nn.Conv3d(c*4, c*4, kernel_size=3, padding=1)
+        self.gn42 = nn.GroupNorm(num_groups=int(c*4/8), num_channels= c*4)
+
+        # Decoderblock 2
+        self.up2 = nn.ConvTranspose3d(c*4, c*4, kernel_size= 2,stride=2, output_padding = 0)
+        self.conv51 = nn.Conv3d(c*4+c*2, c*2, kernel_size=3, padding=1)                         # here additional input_channels for cross connections can be added
+        self.gn51 = nn.GroupNorm(num_groups=int(c*2/8), num_channels= c*2)
+        self.conv52 = nn.Conv3d(c*2, c*2, kernel_size=3, padding=1)
+        self.gn52 = nn.GroupNorm(num_groups=int(c*2/8), num_channels= c*2)
+
+        # Decoderblock 3
+        self.up3 = nn.ConvTranspose3d(c*2, c*2, kernel_size= 2,stride=2, output_padding = 0)
+        self.conv61 = nn.Conv3d(c*2+c, c, kernel_size=3, padding=1)                             # here additional input_channels for cross connections can be added
+        self.gn61 = nn.GroupNorm(num_groups=int(c/8), num_channels= c)
+        self.conv62 = nn.Conv3d(c, c, kernel_size=3, padding=1)
+        self.gn62 = nn.GroupNorm(num_groups=int(c/8), num_channels= c)
+
+        # Output
+        self.conv_end = nn.Conv3d(c,1,kernel_size=1, padding =0, padding_mode='reflect')
+
+    def forward(self,x, timestep):
+
+
+        # Encoder Block 1
+        x = F.gelu(self.gn11(self.conv11(x)))           # gaussian linear unit (gelu) for non-linearity
+        x_cross1 = F.gelu(self.gn12(self.conv12(x)))    # save x_cross<i> for cross connection
+        x = self.avgpool(x_cross1)
+
+        # Encoder Block 2
+        x = F.gelu(self.gn21(self.conv21(x)))
+        x_cross2 = F.gelu(self.gn22(self.conv22(x)))
+        x = self.avgpool(x_cross2)
+
+        # Encoder Block 3
+        x = F.gelu(self.gn31(self.conv31(x)))
+        x_cross3 = F.gelu(self.gn32(self.conv32(x)))
+        x = self.avgpool(x_cross3)
+
+        # Middle forward:
+        x = F.gelu(self.gn2(self.conv2(F.gelu(self.gn1(self.conv1(x))))))
+
+        # Decoder Block 1
+        x = self.up(x)
+        x = torch.cat((x,x_cross3), dim = -4)
+        x = F.gelu(self.gn41(self.conv41(x)))
+        x = F.gelu(self.gn42(self.conv42(x)))
+
+        # Decoder Block 2
+        x = self.up(x)
+        x = torch.cat((x,x_cross2), dim = -4)
+        x = F.gelu(self.gn51(self.conv51(x)))
+        x = F.gelu(self.gn52(self.conv52(x)))
+
+        # Decoder Block 3
+        x = self.up(x)
+        x = torch.cat((x,x_cross1), dim = -4)
+        x = F.gelu(self.gn61(self.conv61(x)))
+        x = F.gelu(self.gn62(self.conv62(x)))
+
+        # Output:
+        x= self.con_end(x)
+
+        # Boundary Condition Padding:
+            # impose dirichlet bc as a padding
+            # Set the front and back slices
+        x[:, :, 0, :, :] = x_input[:, :, 0, :, :]
+        x[:, :, -1, :, :] = x_input[:, :, -1, :, :]
+
+        # Set the top and bottom slices
+        x[:, :, :, 0, :] = x_input[:, :, :, 0, :]
+        x[:, :, :, -1, :] = x_input[:, :, :, -1, :]
+
+        # Set the left and right slices
+        x[:, :, :, :, 0] = x_input[:, :, :, :, 0]
+        x[:, :, :, :, -1] = x_input[:, :, :, :, -1]
+
+
 
 
 
